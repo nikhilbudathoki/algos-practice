d0bd066fc78b28a76d460dd9a353d519418eaff7
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2019-11-15 03:32:22 -0500

    Elementary Sorting Algorithms

---
+++ Sorting/insertion_sort.js
@@ -0,0 +1,27 @@
+/*
+Insertion sort works by creating a left portion that is always sorted
+and keeps getting larger
+*/
+
+
+function swap(arr, indx1, indx2) {
+    let temp = arr[indx1];
+    arr[indx1] = arr[indx2];
+    arr[indx2] = temp;
+}
+
+
+function insertionSort(arr) {
+
+    for (let i = 1; i < arr.length; i++) {
+        var currentVal = arr[i];
+
+        for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+            arr[j + 1] = arr[j];
+        }
+
+        arr[j + 1] = currentVal;
+    }
+    return arr;
+}
+
--- Sorting/selection_sort.js
+++ Sorting/selection_sort.js
@@ -1,7 +1,31 @@
 /*
-Selection Sort
+Selection Sort: similar to bubble sort but instead of first sorting large values,
+places small values into sorted position.
 */
 
-function selectionSort(arr) {
-
+function swap(arr, indx1, indx2) {
+    let temp = arr[indx1];
+    arr[indx1] = arr[indx2];
+    arr[indx2] = temp;
 }
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        var minSoFar = i;
+
+        for (let j = i + 1; j < arr.length; j++) {
+
+            if (arr[j] < arr[minSoFar]) {
+                minSoFar = j;
+            }
+        }
+        if (minSoFar != i) {
+            swap(arr, minSoFar, i);
+        }
+    }
+    return arr;
+}
+
+
+arr = [34, 22, 10, 19, 17];
+console.log(selectionSort(arr));
