85b2fde61f19051e30e8fb58f88a1e0a07631ae6
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2019-11-16 20:23:39 -0500

    Merge Function fix - one array larger than other

--- Sorting/Complex/merge_sort.js
+++ Sorting/Complex/merge_sort.js
@@ -16,6 +16,12 @@ function merge(arr1, arr2) {
             ++j;
         }
     }
+    while (i < arr1.length) {
+        result.push(arr1[i]);
+    }
+    while (j < arr2.length) {
+        result.push(arr2[j]);
+    }
     return result;
 }
 
