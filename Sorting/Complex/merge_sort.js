4044adfd3ba1d59cc7fcd3bb950181dc71de71c9
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2019-11-16 20:17:29 -0500

    Merge Function

---
+++ Sorting/Complex/merge_sort.js
@@ -0,0 +1,29 @@
+/*
+merge accepts two sorted arrays and merges them into a sorted
+larger array
+*/
+function merge(arr1, arr2) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            result.push(arr1[i]);
+            ++i;
+        }
+        else {
+            result.push(arr2[j]);
+            ++j;
+        }
+    }
+    return result;
+}
+
+
+/*
+mergeSort splits up an array into arrays of length 0 or 1,
+then merges those smaller arrays. It sorts while merging.
+*/
+function mergeSort(arr) {
+
+}
