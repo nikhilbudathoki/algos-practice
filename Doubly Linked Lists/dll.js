6a6254f2e2e711dc59969747b865087310a70ba3
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2020-04-13 23:44:53 -0400

    Defined DLL node

--- Doubly Linked Lists/dll.js
+++ Doubly Linked Lists/dll.js
@@ -2,3 +2,11 @@
 Unlike a singly linked list, in a doubly linked list,
 each node has a pointer to next and previous
 */
+
+class Node {
+        constructor(val) {
+                self.val = val;
+                self.next = null;
+                self.prev = null;
+        }
+}
