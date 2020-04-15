3d3f3a0e81f538f78bb6e915d0a81fd0c8c876c9
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2020-04-14 20:47:26 -0400

    DLL push

--- Doubly Linked Lists/dll.js
+++ Doubly Linked Lists/dll.js
@@ -17,5 +17,19 @@ class DoublyLinkedList {
                 this.tail = null;
                 this.length = 0;
         }
+        push(val) {
+                let newNode = new Node(val);
+                if (this.length === 0) {
+                        this.head = newNode;
+                        this.tail = newNode;
+                }
+                else {
+                        this.tail.next = newNode;
+                        newNode.prev = this.tail;
+                        this.tail = newNode;
+                }
+                ++this.length;
+                return this;
+        }
 
 }
