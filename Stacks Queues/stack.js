64753c31724dcb316c5b9bf2e7d40f24fbee4fa3
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2020-04-18 21:16:47 -0400

    Added Node for stack

---
+++ Stacks Queues/stack.js
@@ -0,0 +1,16 @@
+/*
+LIFO: Last-in First Out
+*/
+
+class Node {
+        constructor(value) {
+                this.value = value;
+                this.next = null;
+        }
+}
+
+class Stack {
+        constructor() {
+                this.data = new SinglyLinkedList();
+        }
+}
