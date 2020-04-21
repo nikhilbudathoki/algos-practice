ab3772643833fd1b2593bd3d0f7c0e4013af281f
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2020-04-20 21:48:07 -0400

    BST Insert

--- BST/binary_search_tree.js
+++ BST/binary_search_tree.js
@@ -15,4 +15,43 @@ class BST {
         constructor() {
                 this.root = null;
         }
+
+        insert(val) {
+                let newNode = new Node(val);
+                if (!this.root) {
+                        this.root = newNode;
+                        return this;
+                }
+
+                let current = this.root;
+                while (true) {
+                        if (val < current.value) {
+                                if (!current.left) {
+                                        current.left = newNode;
+                                        return this;
+                                }
+                                current = current.left;
+                                continue;
+                        }
+                        else if (val > current.value) {
+                                if (!current.right) {
+                                        current.right = newNode;
+                                        return this;
+                                }
+                                current = current.right;
+                                continue;
+                        }
+                        if (val = current.val) {
+                                return this;
+                        }
+                }
+        }
 }
+
+
+
+var tree = new BST();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
