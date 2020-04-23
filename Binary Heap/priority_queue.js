47d657d1b94189db4722ad4c22dc6de10f7d50fd
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2020-04-23 00:03:39 -0400

    Priority Queue Template

---
+++ Binary Heap/priority_queue.js
@@ -0,0 +1,74 @@
+/*
+We can store a heap using an array
+*/
+class PriorityQueue {
+        constructor() {
+                this.values = [];
+        }
+        swap(i, j) {
+                let temp = this.values[i];
+                this.values[i] = this.values[j];
+                this.values[j] = temp;
+        }
+        insert(val) {
+                this.values.push(val);
+                let index = this.values.length - 1;
+                while (index > 0) {
+                        let element = this.values[index];
+                        let parentIndex = Math.floor((index - 1) / 2);
+                        let parent = this.values[parentIndex];
+                        if (element <= parent) break;
+                        this.values[parentIndex] = element;
+                        this.values[index] = parent;
+                        index = parentIndex;
+                }
+        }
+        extractMax() {
+                if (this.values.length === 1) {
+                        return this.values.pop();
+                }
+                this.swap(0, this.values.length - 1);
+                let poppedNode = this.values.pop();
+                this.sinkDown();
+                return poppedNode;
+        }
+        sinkDown() {
+                let index = 0;
+                const length = this.values.length;
+                const element = this.values[0];
+                while (true) {
+                        let leftChildIdx = 2 * index + 1;
+                        let rightChildIdx = 2 * index + 2;
+                        let leftChild, rightChild;
+                        let swap = null;
+
+                        if (leftChildIdx < length) {
+                                leftChild = this.values[leftChildIdx];
+                                if (leftChild > element) {
+                                        swap = leftChildIdx;
+                                }
+                        }
+                        if (rightChildIdx < length) {
+                                rightChild = this.values[rightChildIdx];
+                                if (
+                                        (swap === null && rightChild > element) ||
+                                        (swap !== null && rightChild > leftChild)
+                                ) {
+                                        swap = rightChildIdx;
+                                }
+                        }
+                        if (swap === null) break;
+                        this.swap(index, swap);
+                        index = swap;
+                }
+        }
+}
+
+let pQueue = new PriorityQueue();
+pQueue.insert(55);
+pQueue.insert(52);
+pQueue.insert(37);
+
+console.log(pQueue.values);
+pQueue.extractMax();
+console.log(pQueue.values);
