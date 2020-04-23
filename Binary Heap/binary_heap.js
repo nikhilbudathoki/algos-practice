879d3ceb0ef5fd8ab04c4d1dd8964e58ae6f3ef0
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2020-04-22 21:59:25 -0400

    Max Binary Heap Insert

--- Binary Heap/binary_heap.js
+++ Binary Heap/binary_heap.js
@@ -5,4 +5,24 @@ class MaxBinaryHeap {
         constructor() {
                 this.values = [];
         }
+        insert(val) {
+                this.values.push(val);
+                let index = this.values.length - 1;
+                while (index > 0) {
+                        let element = this.values[index];
+                        let parentIndex = Math.floor((index - 1) / 2);
+                        let parent = this.values[parentIndex];
+                        if (element <= parent) break;
+                        this.values[parentIndex] = element;
+                        this.values[index] = parent;
+                        index = parentIndex;
+                }
+        }
 }
+
+let heap = new MaxBinaryHeap();
+heap.insert(55);
+heap.insert(52);
+heap.insert(37);
+
+console.log(heap.values);
