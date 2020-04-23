c11583faa24e54edf19a0ad477a4291fcbb6358f
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2020-04-22 23:37:36 -0400

    Binary Heap Extract Max

--- Binary Heap/binary_heap.js
+++ Binary Heap/binary_heap.js
@@ -18,6 +18,43 @@ class MaxBinaryHeap {
                         index = parentIndex;
                 }
         }
+        extractMax() {
+                function swap(arr, i, j) {
+                        let temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                }
+                swap(this.values, 0, this.values.length - 1);
+                let poppedNode = this.values.pop();
+                let index = 0;
+                while (true) {
+                        let parent = this.values[index];
+                        let leftIdx = 2 * index + 1;
+                        let rightIdx = 2 * index + 2;
+                        let lChild = leftIdx > this.values.length ? null : this.values[leftIdx];
+                        let rChild = rightIdx > this.values.length ? null : this.values[rightIdx];
+                        if (!(lChild || rChild)) break;
+                        if (lChild > parent && rChild > parent) {
+                                if (lChild > rChild) {
+                                        swap(this.values, leftIdx, index);
+                                }
+                                if (lChild <= rChild) {
+                                        swap(this.values, rightIdx, index);
+                                }
+                                continue;
+                        }
+                        if (lChild > parent) {
+                                swap(this.values, leftIdx, index);
+                        }
+                        else if (rChild > parent) {
+                                swap(this.values, rightIdx, index);
+                        }
+                        else {
+                                break;
+                        }
+                }
+                return poppedNode;
+        }
 }
 
 let heap = new MaxBinaryHeap();
@@ -25,4 +62,6 @@ heap.insert(55);
 heap.insert(52);
 heap.insert(37);
 
+console.log(heap.values);
+heap.extractMax();
 console.log(heap.values);
