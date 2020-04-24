2cfdff8f8a6c4b887edb040785cd0ded9120ad9f
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2020-04-23 22:19:56 -0400

    Implemented hashing function for HT

--- Hash Table/hash_table.js
+++ Hash Table/hash_table.js
@@ -4,20 +4,20 @@ Important concepts:
 - Linear Probing
 - Collisions in hash table
 */
-function hash(key, numBuckets) {
-        let total = 0;
-        let WEIRD_PRIME = 31;
-        for (let i = 0; i < Math.min(key.length, 100); ++i) {
-                let char = key[i];
-                let value = char.charCodeAt(0) - 96;
-                total += (total * WEIRD_PRIME + value) % numBuckets;
-        }
-        return total;
-}
-
 
 class HashTable {
         constructor(size = 53) {
                 this.keyMap = new Array(size);
         }
+        _hash(key) {
+                let total = 0;
+                let WEIRD_PRIME = 31;
+                for (let i = 0; i < Math.min(key.length, 100); ++i) {
+                        let char = key[i];
+                        let value = char.charCodeAt(0) - 96;
+                        total += (total * WEIRD_PRIME + value) % this.keyMap.length;
+                }
+                return total;
+        }
+
 }
