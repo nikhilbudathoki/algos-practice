de42d92d27e1f324d94ea0bea659e0f69ac34497
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2020-04-23 21:29:48 -0400

    Basic modulo  utf-8 hash

--- Hash Table/hash_table.js
+++ Hash Table/hash_table.js
@@ -4,3 +4,11 @@ Important concepts:
 - Linear Probing
 - Collisions in hash table
 */
+function hash(key, arrayLen) {
+        let total = 0;
+        for (let char of key) {
+                let value = char.charCodeAt(0) - 96;
+                total += (total + value) % arrayLen;
+        }
+        return total;
+}
