0b2b0a92062eabe40006874895fbafa8297104a1
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2020-04-26 12:20:51 -0400

    Graph removeVertex()

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -6,22 +6,36 @@ We will use adjacency lists to model graphs
 
 // Building an undirected graph
 class Graph {
-        constructor() {
-                this.adjanceyList = {};
-        }
-        addVertex(name) {
-                if (!this.adjanceyList[name]) this.adjanceyList[name] = [];
-        }
+	constructor() {
+		this.adjanceyList = {};
+	}
+	addVertex(name) {
+		if (!this.adjanceyList[name]) this.adjanceyList[name] = [];
+	}
 
-        addEdge(v1, v2) {
-                this.adjanceyList[v1].push(v2); // For directed and undirected
-                this.adjanceyList[v2].push(v1); // Only undirected graphs
-        }
+	addEdge(v1, v2) {
+		this.adjanceyList[v1].push(v2); // For directed and undirected
+		this.adjanceyList[v2].push(v1); // Only undirected graphs
+	}
 
-        removeEdge(v1, v2) {
-                this.adjanceyList[v1] = this.adjanceyList[v1].filter(item => item !== v2);
-                this.adjanceyList[v2] = this.adjanceyList[v2].filter(item => item !== v1);
-        }
+	removeEdge(v1, v2) {
+		this.adjanceyList[v1] = this.adjanceyList[v1].filter(
+			item => item !== v2
+		);
+		this.adjanceyList[v2] = this.adjanceyList[v2].filter(
+			item => item !== v1
+		);
+	}
+
+	removeVertex(vertex) {
+		let edge = this.adjanceyList[vertex];
+		for (let node in edge) {
+			this.removeEdge(vertex, node);
+		}
+		delete this.adjanceyList[vertex];
+	}
+
+
 }
 
 let g = new Graph();
@@ -29,6 +43,7 @@ g.addVertex("Tokyo");
 g.addVertex("Dallas");
 g.addVertex("Aspen");
 g.addEdge("Dallas", "Tokyo");
+g.addEdge("Dallas", "Aspen");
 console.log(g.adjanceyList);
 g.removeEdge("Dallas", "Tokyo");
 console.log(g.adjanceyList);
