3174d05bf48a2aa4336fcc10efd1242b97211104
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2020-04-26 12:06:05 -0400

    Graph addEdge()

--- Graphs/graph.js
+++ Graphs/graph.js
@@ -12,8 +12,14 @@ class Graph {
         addVertex(name) {
                 if (!this.adjanceyList[name]) this.adjanceyList[name] = [];
         }
+
+        addEdge(v1, v2) {
+                this.adjanceyList[v1].push(v2);
+                this.adjanceyList[v2].push(v1);
+        }
 }
 
 g = new Graph();
-g.addVertex("Tokyo")
+g.addVertex("Tokyo");
+g.addVertex("San Francisco");
 console.log(g.adjanceyList);
