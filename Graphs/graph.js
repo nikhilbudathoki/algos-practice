e8719648547f24d506a469e2fddb16177926447f
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2020-04-25 21:14:18 -0400

    Set up node for graph

---
+++ Graphs/graph.js
@@ -0,0 +1,10 @@
+/*
+Graphs are similar to trees
+Each node can be linked to other nodes
+*/
+class Node {
+        constructor(val) {
+                this.val = val;
+                this.connections = [];
+        }
+}
