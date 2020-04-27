4ff2746d8d6beb380bb7695b9846bbb9d374d729
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2020-04-27 12:55:35 -0400

    Fibonacci Bottom Up

--- Dynamic Programming/dynamic.js
+++ Dynamic Programming/dynamic.js
@@ -8,20 +8,35 @@ What we know: Fib(n) = Fib(n - 1) + Fib(n - 2)
 
 
 // recursive solution(BAD)
-function fib_recursive(n) {
+function fibRecursive(n) {
     if (n <= 2) {
         return 1;
     }
-    return fib_recursive(n - 1) + fib_recursive(n - 2);
+    return fibRecursive(n - 1) + fibRecursive(n - 2);
 }
 
 // memoized solution
-function fib_memo(n, memo = []) {
+function fibMemo(n, memo = []) {
+    // In JS, out of index in array is undefined
     if (memo[n] !== undefined) return memo[n];
     if (n <= 2) return 1;
-    let result = fib_memo(n - 1, memo) + fib_memo(n - 2, memo);
+    let result = fibMemo(n - 1, memo) + fibMemo(n - 2, memo);
+    // In JS, can assign index out of array's length
     memo[n] = result;
     return result;
 }
 
-console.log(fib_memo(40));
+// bottom up approach
+function fibBottomup(n) {
+    if (n <= 2) return 1;
+    let fibNums = [0, 1, 1];
+    for (let i = 3; i <= n; i++) {
+        // fibNums[] stores results instead of call stack
+        fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+    }
+    return fibNums[n];
+}
+
+//console.log(fibRecursive(40));
+//console.log(fibMemo(40));
+console.log(fibBottomup(40));
