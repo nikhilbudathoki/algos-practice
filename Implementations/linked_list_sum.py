5e2f9b1e9ac18fefa51581aad4f8a86d6aa46407
Nikhil <43186657+nikhilbudathoki@users.noreply.github.com>
2020-05-02 21:00:40 -0400

    Sum of nums in Linked List documentation

--- Implementations/linked_list_sum.py
+++ Implementations/linked_list_sum.py
@@ -1,3 +1,19 @@
+"""
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order and each of their nodes contain a single digit. 
+Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
 def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
     num_1 = 0
     num_2 = 0
