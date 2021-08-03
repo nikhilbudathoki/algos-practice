0aae0322a9c5d2a90cc7aa0d65ec6b8011baa99b
Nikhil Budathoki <43186657+nikhilbudathoki@users.noreply.github.com>
2021-08-03 10:21:38 -0400

    subsets bitmask

--- Implementations/subsets.py
+++ Implementations/subsets.py
@@ -8,22 +8,39 @@ Source: https://leetcode.com/problems/subsets/
 from typing import List
 
 def subsets(nums: List[int]) -> List[List[int]]:
+    def backtrack(first, curr):
+        # if the combination is done
+        if len(curr) == comb_length:  
+            output.append(curr[:])
+            return
+        for i in range(first, n):
+            # add nums[i] into the current combination
+            curr.append(nums[i])
+            # use next integers to complete the combination
+            backtrack(i + 1, curr)
+            # backtrack
+            curr.pop()
+    
+    output = []
+    first = 0
+    curr = []
+    n = len(nums)
+    for comb_length in range(n + 1):
+        backtrack(first, curr)
+    return output
+
+def subsets_bitmask(nums: List[int]) -> List[List[int]]:
+    n = len(nums)
     result = []
-    if len(nums) == 0:
-        result.append([])
-    else:
-        nums_copy = nums.copy()
-        first_element = nums_copy.pop(0)
-        curr_subsets = [x for x in subsets(nums_copy)]
-        result += [x for x in curr_subsets]
-        result += [[first_element, *x] for x in curr_subsets]
+    for i in range(2**n, 2**(n+1)):
+        bitmask = bin(i)[3:]
+
+        result.append([nums[j] for j in range(n) if bitmask[j] == '1'])
     return result
-
-
-
-
+    
 nums = [1,2,3]
 result = subsets(nums)
-
+result_bitmask = subsets_bitmask(nums)
 print(result)
+print(result_bitmask)
 
