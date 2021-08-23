8bc01ea9991930f2c9989c13033f081c620a0fc7
Nikhil Budathoki <43186657+nikhilbudathoki@users.noreply.github.com>
2021-08-23 13:40:36 -0400

    Tries

---
+++ Implementations/Tries.py
@@ -0,0 +1,45 @@
+"""
+Implement a Trie(prefix-tree)
+"""
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def insert(self, word: str) -> None:
+        cur = self.root 
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = TrieNode()  
+            cur = cur.children[char]
+        cur.end_of_word = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        cur = self.root
+        for char in word:
+            if char not in cur.children:
+                return False
+            cur = cur.children[char]
+        return cur.end_of_word
+        
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        cur = self.root
+        for char in prefix:
+            if char not in cur.children:
+                return False
+            cur = cur.children[char]
+        return True
+
+        
